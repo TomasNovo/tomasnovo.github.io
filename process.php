ea2b96558ab7208e33191d2bcfd6245cbf9eff77
Tomás Novo <tomasnovo98@gmail.com>
2020-07-15 16:48:44 +0100

    Testing contacts feature

---
+++ process.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $subject =  $_POST['subject'];;
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "tomasnovo98@gmail.com";
+
+    $headers = "From: ".$email;
+    $text  = "You have received an email from ".$name.".\n\n".$message;
+    
+    mail($to, $subject, $text, $headers);
+}
+
+
+?>
