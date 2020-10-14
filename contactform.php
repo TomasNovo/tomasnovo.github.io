d711e0eaf2a446e3fa403a7ac5b310759187982e
ReleaseTheKraken <tomasnovo98@gmail.com>
2020-10-14 03:11:18 +0200

    php not working. temporary solution implemented

--- contactform.php
+++ contactform.php
@@ -1,7 +1,29 @@
 <?php 
 
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+echo "You have CORS!";
+
 if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']))
 {
+    print_r("aaaaaa");
     $n = $_POST['n'];
     $e = $_POST['e'];
     $m = nl2br($_POST['m']);
